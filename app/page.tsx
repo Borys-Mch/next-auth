fb9fb6f34d20fba6ff8997d42f203e152cb8713e
Borys Mch <borys.mch@gmail.com>
2024-12-17 13:16:45 +0200

    Created Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,24 @@
-
+import { LoginButton } from "@/components/auth/loginButton";
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
-    <p>Hello Auth</p>
+    <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+      <div className='space-y-6 text-center'>
+        <h1 className="text-white text-6xl font-semibold drop-shadow-md">
+          Auth
+        </h1>
+        <p className="text-white text-lg">
+          A simple authentication service
+        </p>
+        <div>
+          <LoginButton>
+            <Button variant='secondary' size='lg'>
+              Sign In
+            </Button>
+          </LoginButton>
+        </div>
+      </div>
+    </main>
   );
 }
